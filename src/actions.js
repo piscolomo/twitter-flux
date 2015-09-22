13055c420293f3b317b33118a6eb779093717aae
Julio Lopez <ljuliom@gmail.com>
2015-09-22 00:53:05 -0500

    dispatcher, actions and constants

---
+++ src/actions.js
@@ -0,0 +1,16 @@
+var dispatcher = require('./dispatcher');
+var constants = require('./constants');
+
+Object.keys(constants).forEach(function(key){
+  var funcName = key.split('_').map(function(word, i){
+    if (i===0) return word.toLowerCase();
+    return word[0] + word.slice(1).toLowerCase();
+  }).join("");
+
+  exports[key] = function(data){
+    dispatcher.dispatch({
+      actionType: constants[key],
+      data: data
+    });
+  }
+});
